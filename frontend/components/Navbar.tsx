cfc34878884f918b46694e7df20fda279ab8d87f
AlvinDHacker <alvindsouza2204@gmail.com>
2024-03-30 11:28:51 +0530

    Home Page

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,79 @@
+import { Mail, Phone, Scroll } from "lucide-react";
+import React from "react";
+// import logo  from '../public/LearnBlocksLogo.png'
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="bg-black opacity-90 border-gray-200 ">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="/"
+            className="flex items-center text-white space-x-3 rtl:space-x-reverse"
+          >
+            <Image src='/public/LearnBlocksLogo.png' height={30} width={30} alt="logo"/>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+              Learn <span className="text-lb-pink">Blocks</span>
+            </span>
+          </a>
+
+          <ul className="flex bg-black flex-row md:hidden">
+          <li>
+              <a
+                href="tel:9820257477"
+                className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+              >
+                <Phone />
+              </a>
+            </li>
+            <li>
+              <a
+                href="AlvinDsouza_Resume.pdf"
+                target="blank"
+                className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+              >
+                <Scroll />
+              </a>
+            </li>
+          </ul>
+
+          <div
+            className="hidden bg-black w-full  md:block md:w-auto"
+            id="navbar-default"
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-900 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-black ">
+            <li>
+                <a
+                  href="tel:9820257477"
+                  className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                >
+                  <Phone />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="mailto:alvindsouza2204@gmail.com"
+                  className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                >
+                  <Mail />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="AlvinDsouza_Resume.pdf"
+                  target="blank"
+                  className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                >
+                  <Scroll />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,13 +1,52 @@
 import type { Config } from "tailwindcss";
 
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config: Config = {
   content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        'lb-purple': '#6F3284',
+        'lb-pink': '#F487D2',
+      },
+      animation: {
+        "meteor-effect": "meteor 5s linear infinite",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
+        
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +54,18 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
+  
 };
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
