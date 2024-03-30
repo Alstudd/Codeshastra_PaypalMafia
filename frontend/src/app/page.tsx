acf8404f331df2fdd3e8c52d62ecd44432a1e1a9
Joyvin <joymendonca710@gmail.com>
2024-03-30 18:20:22 +0530

    addded waller

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,13 +6,13 @@ import CreateCourse from "../../components/CreateCourse";
 import { useTheme } from "next-themes";
 
 export default function Home() {
-  const { setTheme } = useTheme();
-  setTheme("dark");
-  return (
-    <>
-    <Navbar/>
-    {/* <HeroParallaxDemo/> */}
-    <CreateCourse/>
-    </>
-  );
+	const { setTheme } = useTheme();
+	setTheme("dark");
+	return (
+		<>
+			<Navbar />
+			{/* <HeroParallaxDemo/> */}
+			<CreateCourse />
+		</>
+	);
 }
